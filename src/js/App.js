f78026f9fa68a8cdbfca212538676b0a1725159f
Tyler Mathern <math3rn@gmail.com>
2020-04-13 17:28:17 -0600

    Adding website code

---
+++ src/js/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Body from './Body';
+
+function App() {
+
+  return (
+    <div className="App"> 
+      <Header />
+      
+      <Body />
+      
+      <Footer />
+    </div>
+  );
+}
+export default App;
